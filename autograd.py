3287f3c744c3f60d3765a73c108d7e6be33dbf78
Sasha Rush <srush.research@gmail.com>
2022-05-11 09:40:01 -0400

    update

---
+++ autograd.py
@@ -0,0 +1,55 @@
+from base import *
+
+right_arrow = make_path([(0, 0), (1, 0)], True).line_width(0.03).center_xy()
+left_arrow = make_path([(1, 0), (0, 0)], True).line_width(0.03).center_xy()
+box = rectangle(1.5, 1).line_width(0.05).fill_color(grey)
+bbox = rectangle(1.5, 1).line_width(0.05).fill_color(papaya)
+def ta(text):
+    return t(text, 0.3)
+
+
+d = right_arrow.named("in") | box | right_arrow.named("out")
+d += label_above(d, "in", ta("x"))
+d += label_above(d, "out", ta("f(x)"))
+
+d.render_svg("diagrams/Autograd/autograd1.svg", 20)
+
+
+d = (right_arrow.named("x") / vstrut(0.8) / right_arrow.named("y")).center_xy() | box | right_arrow.named("out")
+d += label_above(d, "x", ta("x"))
+d += label_below(d, "y", ta("y"))
+d += label_above(d, "out", ta("f(x, y)"))
+
+d.render_svg("diagrams/Autograd/autograd2.svg", 20)
+
+d = left_arrow.named("out") | bbox | left_arrow.named("in")
+d += label_above(d, "out", ta("f'(x)"))
+d += label_above(d, "in", ta("d_out"))
+
+d.render_svg("diagrams/Autograd/autograd3.svg", 20)
+
+d = (left_arrow.named("x") / vstrut(0.8) / left_arrow.named("y")).center_xy() | bbox | left_arrow.named("out")
+d += label_above(d, "x", ta("f_x'(x, y)"))
+d += label_below(d, "y", ta("f_y'(x, y)"))
+d += label_above(d, "out", ta("d_out"))
+
+d.render_svg("diagrams/Autograd/autograd4.svg", 20)
+
+
+
+d = (right_arrow / vstrut(0.8) / right_arrow).center_xy() | box.named("l") | hstrut(1) | (box.named("t") / vstrut(1) / box.named("b")).center_xy() | hstrut(1) | box.named("r") | right_arrow 
+
+d += connect_outer(d, "l", "E", "t", "W").line_width(0.03)
+d += connect_outer(d, "l", "E", "b", "W").line_width(0.03)
+d += connect_outer(d, "t", "E", "r", "W").line_width(0.03)
+d += connect_outer(d, "b", "E", "r", "W").line_width(0.03)
+
+d.render_svg("diagrams/Autograd/autograd5.svg", 20)
+
+
+d = right_arrow.named("in") | box | right_arrow.named("out") | box | right_arrow.named("out2")
+d += label_above(d, "in", ta("x"))
+d += label_above(d, "out", ta("g(x)"))
+d += label_above(d, "out2", ta("f(g(x))"))
+
+d.render_svg("diagrams/Autograd/chain1.svg", 20)
---
+++ base.py
@@ -0,0 +1,66 @@
+from colour import Color
+from diagrams import *
+from diagrams.bounding_box import BoundingBox
+
+# Library 
+
+# Colors
+papaya = Color("#ff9700")
+blue = Color("#005FDB")
+olive = Color("#708238")
+black = Color("#000000")
+white = Color("#ffffff")
+grey = Color("#bbbbbb")
+
+# Text
+def t(te, s=1):
+    return text(te, s).fill_color(black).line_color(white)
+
+
+# Tiling 
+def tile(d, m, n, name = ""):
+    return hcat(vcat(d.named((name, j, i)) for j in range(n)) for i in range(m)).center_xy()
+
+# Define spacing
+
+def hstrut(width=0.2): return hrule(width).line_width(0)
+def vstrut(height=0.5): return vrule(height).line_width(0)
+
+v = vstrut(2.5)
+v2 = vstrut(1.25)
+h = hstrut(2.5)
+
+def pad(d, x):
+    return d.pad_l(x).pad_r(x).pad_t(x).pad_b(x)
+
+
+# Arrows
+
+# Arrow Labels
+def label(t):
+    return circle(0.5).fill_color(white).line_color(white) + text(t, 0.5).fill_color(black)
+
+
+# Label above
+
+def label_above(d, name, label):
+    b = d.get_subdiagram_bounding_box(name)
+    return label.translate(b.center.x, b.top - label.get_bounding_box().height)
+
+def label_below(d, name, label):
+    b = d.get_subdiagram_bounding_box(name)
+    return label.translate(b.center.x, b.bottom + label.get_bounding_box().height)
+
+
+def surround(diagram):
+    bb = diagram.get_bounding_box()
+    rect = rectangle(bb.width + 2, bb.height + 2, 1.0)
+    return rect + diagram.center_xy()
+
+def cover(d, a, b):
+    b1 = d.get_subdiagram_bounding_box(a)
+    b2 = d.get_subdiagram_bounding_box(b)
+    new_bb = BoundingBox(b1.tl, b2.br)
+    return rectangle(new_bb.width, new_bb.height, 0.0) \
+            .translate(new_bb.center.x, new_bb.center.y)
+
